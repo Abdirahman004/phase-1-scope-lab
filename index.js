da26efbd6c317f084f276318c8ea1aad8e057b66
Abdirahman004 <abdirahman.alio@student.moringaschool.com>
2024-07-01 23:40:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+//  Declare a global variable customerName
+var customerName = 'bob';
+
+//  Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+//  Function to set bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+//  Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+//   Declare a constant leastFavoriteCustomer
+const leastFavoriteCustomer = 'some initial value';
+
+// Function to attempt to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'a new value'; 
+}
+
